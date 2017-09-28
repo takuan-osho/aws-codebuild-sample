63981a5b81926e5e196e32a9f0021e798fd4f573
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-09-28 16:18:57 +0900

    [WIP] Fix file name and IAM role name

--- aws/ami/iam.tf
+++ aws/ami/iam.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_role" "codebuild_sample" {
     name = "codebuild_sample_role"
-    assume_role_policy = "${data.template_file.iam-role-principal.rendered}"
+    assume_role_policy = "${data.template_file.codebuild-sample-role.rendered}"
 }
 
 resource "aws_iam_role_policy" "codebuild_sample" {
     name = "codebuild_sample_policy"
     role = "${aws_iam_role.codebuild_sample.id}"
-    policy = "${data.template_file.iam-role-default.rendered}"
+    policy = "${data.template_file.codebuild-sample-role-policy.rendered}"
 }
 
 resource "aws_iam_role" "codepipeline_sample" {
--- aws/ami/template_file.tf
+++ aws/ami/template_file.tf
@@ -1,9 +1,9 @@
-data "template_file" "iam-role-principal" {
-  template = "${file("${path.module}/policy/iam-role-principal.tpl")}"
+data "template_file" "codebuild-sample-role" {
+  template = "${file("${path.module}/policy/codebuild-sample-role.tpl")}"
 }
 
-data "template_file" "iam-role-default" {
-  template = "${file("${path.module}/policy/iam-role-default.tpl")}"
+data "template_file" "codebuild-sample-role-policy" {
+  template = "${file("${path.module}/policy/codebuild-sample-role-policy.tpl")}"
 }
 
 data "template_file" "codepipeline-sample-role" {
