4e876563203256b49ec51d2fbfd44c5e933f2785
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-09-29 11:40:51 +0900

    Change timeout value

--- aws/ami/codebuild.tf
+++ aws/ami/codebuild.tf
@@ -1,7 +1,6 @@
 resource "aws_codebuild_project" "codebuild_sample" {
   name          = "codebuild_sample"
   description   = "codebuild sample project"
-  build_timeout = "10"
   service_role  = "${aws_iam_role.codebuild_sample.arn}"
 
   artifacts {
